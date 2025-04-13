b5abdd94028149c2ca3ee9e6c9a3e061dcf6b821
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2025-04-12 21:55:05 -0300

    chore: initi project

---
+++ src/main/java/me/fio/banco_dev_week/BancoDevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.fio.banco_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/fio/banco_dev_week/BancoDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fio.banco_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
